df5f805225fb4196ae31baeab0c80399b868c7b3
Agustindlf <dlfagustin@hotmail.com>
2021-06-16 19:37:17 +0200

    welcomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Menu from "../src/components/template/menu/menu.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "../src/components/template/footer/footer"
+import Footer from "../src/components/template/footer/footer";
+import Welcome from "./components/users/welcome/welcomePage";
 
 function App() {
   return (
     <div>
       <Menu />
+      <Welcome />
       <Footer/>
     </div>
   );
