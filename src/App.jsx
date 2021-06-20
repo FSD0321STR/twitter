61e64b7e820321c62100b59e575a509073ce77e2
Jose Juan Ortega <josejuan.ortega7@gmail.com>
2021-06-20 10:08:18 +0200

    tweets feed actualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Menu from "../src/components/template/menu/menu.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "../src/components/template/footer/footer"
+import Footer from "../src/components/template/footer/footer";
+import Feed from "../src/components/template/tweets/list/feed/feed.jsx";
 
 function App() {
   return (
     <div>
       <Menu />
-      <Footer/>
+      <Feed />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/template/tweets/list/feed/tweetsFeed.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function tweetsFeed() {
+  return (
+    <div>
+      <p>Hola</p>
+      <p>Hola</p>
+      <p>Hola</p>
+      <p>Hola</p>
+      <p>Hola</p>
+      <p>Hola</p>
+      <p>Hola</p>
+    </div>
+  );
+}
+
+export default tweetsFeed;
