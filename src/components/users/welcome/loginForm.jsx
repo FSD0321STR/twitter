23391bf6f2ed8c141274309a0452a5752b2c14ed
PedroLuM <pedroproissues@gmail.com>
2021-06-21 19:49:14 +0200

    backStuff

--- src/components/users/welcome/loginForm.jsx
+++ src/components/users/welcome/loginForm.jsx
@@ -1,30 +1,46 @@
-import React from "react";
-import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 import LoadingButton from "./loadingButton";
+import { useHistory } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
+function LoginForm() {
+  const { login } = useAuth();
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function LoginForm(){
-    return(
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      Please provide your mail.
-    </Form.Text>
-  </Form.Group>
+  function handleSubmit(event) {
+    event.preventDefault();
+    login({ email, password });
+    history.push("/Pedro");
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter email"
+        />
+        <Form.Text className="text-muted">Please provide your mail.</Form.Text>
+      </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Remember me" />
-  </Form.Group>
-  <LoadingButton />
-</Form>
-
-    )
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Remember me" />
+      </Form.Group>
+      <LoadingButton />
+    </Form>
+  );
 }
 
 export default LoginForm;
